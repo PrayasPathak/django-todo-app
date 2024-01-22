387ba9bc74d5c928f650a8f694da856b47ac89dc
PrayasPathak <prayasroman1@gmail.com>
2024-01-22 12:52:45 +0545

    Added update, delete and view functionality to the project.

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("add/", views.add_task, name="add"),
+    path("update/<str:id>/", views.update_task, name="update_task"),
+    path("delete/<str:id>/", views.delete_task, name="delete_task"),
+    path("view/<str:id>/", views.view_task, name="view_task"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -14,6 +14,12 @@ def index(request):
     return render(request, "todos/index.html", context)
 
 
+def view_task(request, id):
+    task = Task.objects.get(id=id)
+    context = {"task": task}
+    return render(request, "todos/view_task.html", context)
+
+
 def add_task(request):
     form = AddTaskForm()
     if request.method == "POST":
@@ -23,3 +29,24 @@ def add_task(request):
             return redirect("home")
     context = {"form": form}
     return render(request, "todos/add_task.html", context)
+
+
+def update_task(request, id):
+    task = Task.objects.get(id=id)
+    form = AddTaskForm(instance=task)
+    if request.method == "POST":
+        form = AddTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, "todos/update_task.html", context)
+
+
+def delete_task(request, id):
+    task = Task.objects.get(id=id)
+    if request.method == "POST":
+        task.delete()
+        return redirect("home")
+    context = {"task": task}
+    return render(request, "todos/delete_task.html", context)
